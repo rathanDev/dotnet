298c355c95568de09ad2e869df279ca38f8d2054
Jana <rathan.kparam@gmail.com>
2026-01-27 06:48:22 +0800

    Init WebApi

---
+++ MyWebApi2026/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApi2026.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Greet()
+    {
+        return Ok("Hi!");
+    }
+
+    [HttpGet("{name}")]
+    public IActionResult GreetWithName(string name)
+    {
+        return Ok($"Hi {name}!");
+    }
+
+}

add9926aba11ae62bffcdf57a5943bea6e779b88
Jana <rathan.kparam@gmail.com>
2026-01-27 20:48:47 +0800

    Implement greet controller and jwt validation in api gw

---
+++ MyMicroservices/ApiGateway/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Greet()
+    {
+        return Ok("Hello there!");
+    }
+
+}
--- MyMicroservices/ApiGateway/Program.cs
+++ MyMicroservices/ApiGateway/Program.cs
@@ -1,12 +1,45 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
+
+var jwt = builder.Configuration.GetSection("Jwt");
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwt["Issuer"],
+            ValidAudience = jwt["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(jwt["Key"]!)
+            )
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+
+builder.Services.AddControllers();
+
+
 builder.Services
     .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 
 var app = builder.Build();
 
@@ -38,6 +71,16 @@ app.UseHttpsRedirection();
 //    return forecast;
 //})
 //.WithName("GetWeatherForecast");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapReverseProxy();
+
+
+app.MapControllers();
+
 
 app.Run();
 

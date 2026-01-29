7a195c69773615a8213d5e67949da86cf9f1efb7
Jana <rathan.kparam@gmail.com>
2026-01-29 10:20:00 +0800

    Implement ProdSvc

---
+++ MyMicroservices/ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetProducts()
+    {
+        var products = new[]
+        {
+            new { Id = 1, Name = "Product A", Price = 10.0 },
+            new { Id = 2, Name = "Product B", Price = 20.0 },
+            new { Id = 3, Name = "Product C", Price = 30.0 }
+        };
+        return Ok(products);
+    }
+
+}
--- MyMicroservices/UserService/Program.cs
+++ MyMicroservices/UserService/Program.cs
@@ -46,7 +46,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 //var summaries = new[]
 //{
